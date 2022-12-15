06b2e6d2bd25cf32eb87239e2f182574a82b2b02
Gabriel Feldman <testeosypracticas@gmail.com>
2022-12-15 20:25:49 -0300

    actualizacion form alta, validaciones y drag and drop

--- public/js/menu/alta.js
+++ public/js/menu/alta.js
@@ -2,20 +2,26 @@ class FormularioAlta {
     inputs = null;
     form = null;
     button = null ;
-    camposValidos = [false, false, false, false, false, false, false];
-
+    camposValidos = [false, false, false, false, false, false]
+    
     regExpValidar = [
-    /^.+$/, // regexp nombre
-    /^.+$/, // regexp precio
-    /^[0-9]+$/, // regexp stock
-    /^.+$/, // regexp marca
-    /^.+$/, // regexp categoria
-    /^.+$/, // regexp detalles
-    /^.+$/ // regexp foto
+      /^.+$/, // regexp nombre
+      /^.+$/, // regexp precio
+      /^[0-9]+$/, // regexp stock
+      /^.+$/, // regexp marca
+      /^.+$/, // regexp categoria
+      /^.+$/, // regexp detalles
     ]
 
+    /* -------------- drag and drop ----------------- */
+    imagenSubida = '';
+    dropArea= null;
+    progressBar= null;
+    /* -------------- drag and drop ----------------- */
+
     constructor(renderTablaAlta, guardarProducto){
-        this.inputs = document.querySelectorAll('main form input');
+        this.inputs = document.querySelectorAll('main form input.data-validation');
+        //console.log(this.inputs)
         this.form = document.querySelector('main form');
         this.button = document.querySelector('main form button');
         
@@ -40,6 +46,48 @@ class FormularioAlta {
             if(guardarProducto) guardarProducto(producto);
             
         })
+         /* -------------- drag and drop ----------------- */
+        this.dropArea = document.getElementById('drop-area');
+        this.progressBar = document.getElementById('progress-bar');
+
+        // Para cancelar el evento automático del drag and drop
+
+        ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+            this.dropArea.addEventListener(eventName, e => e.preventDefault());
+            document.body.addEventListener(eventName, e => e.preventDefault());
+        })
+
+        // Para remarcar la zona de drop al arrastrar
+        ;['dragenter', 'dragover'].forEach(eventName => {
+            this.dropArea.addEventListener(eventName, () =>{
+                this.dropArea.classList.add('highlight')
+            })
+        }) 
+
+        ;['dragleave', 'drop'].forEach(eventName => {
+            this.dropArea.addEventListener(eventName, () =>{
+                this.dropArea.classList.remove('highlight')
+            })
+        }) 
+
+        this.dropArea.addEventListener('drop', e => {
+            const dataTransf = e.dataTransfer;
+            const files = dataTransf.files;
+
+            this.handleFiles(files)
+        })
+         /* -------------- drag and drop ----------------- */
+    
+         const inputFoto = document.querySelector('#foto')
+
+         inputFoto.addEventListener('change', () => {
+           //console.log('Cambió el input')
+     
+           const files = inputFoto.files
+     
+           this.handleFiles(files)
+         })
+
     }
 
     // Para comprobar la validez de los campos
@@ -50,15 +98,14 @@ class FormularioAlta {
             this.camposValidos[2] && 
             this.camposValidos[3] && 
             this.camposValidos[4] && 
-            this.camposValidos[5] && 
-            this.camposValidos[6] 
+            this.camposValidos[5]  
             
         return !valido 
     }
     
     // Validar campos
     validar(valor, validador, index) {
-   
+           
     if(!validador.test(valor)) {
         this.setCustomValidityJS('Este campo no es válido', index);
         this.camposValidos[index] = false;
@@ -75,7 +122,7 @@ class FormularioAlta {
 
     // Mostrar u ocultar el mensaje
     setCustomValidityJS(mensaje, index) {
-        let divs = document.querySelectorAll('form div');
+        let divs = document.querySelectorAll('form .mensaje-validacion');
         divs[index].innerHTML = mensaje;
         divs[index].style.display = mensaje ? 'block' : 'none';
     }
@@ -89,8 +136,8 @@ class FormularioAlta {
             marca: this.inputs[3].value,
             categoria: this.inputs[4].value,
             detalles: this.inputs[5].value,
-            foto: this.inputs[6].value,
-            envio: this.inputs[7].checked
+            foto: this.imagenSubida ? `/uploads/${this.imagenSubida}` : '/uploads/sinimagen.jpg',
+            envio: this.inputs[6].checked
         }
 
     }
@@ -104,8 +151,65 @@ class FormularioAlta {
         })
 
         this.button.disabled = true;
-        this.camposValidos = [false, false, false, false, false, false, false];
+        this.camposValidos = [false, false, false, false, false, false];
+
+        const img = document.querySelector('#gallery img');
+        img.src = '';
+
+        this.initializeProgress();
+        this.imagenSubida = '';
     }
+
+   /* -------------- drag and drop ----------------- */
+    initializeProgress() {
+        this.progressBar.value = 0;
+    }
+
+    updateProgress(porcentaje) {
+        this.progressBar.value = porcentaje;
+    }
+
+    previewFile(file) {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = function() {
+        const img = document.querySelector('#gallery img');
+        img.src = reader.result
+        }
+    }
+
+    handleFiles = files => {
+        const file = files[0];
+        this.initializeProgress();
+        this.uploadFile(file);
+        this.previewFile(file);
+    }
+
+    uploadFile = file => {
+        const url = '/api/upload'
+
+        const xhr = new XMLHttpRequest();
+        const formData = new FormData();
+
+        xhr.open('POST', url);
+
+        xhr.upload.addEventListener('progress', e => {
+            let porcentaje = (((e.loaded * 100.0) / e.total) || 100)
+            this.updateProgress(porcentaje)
+        })
+
+        xhr.addEventListener('load', () => { // readyState === 4
+            if ( xhr.status === 200) {
+                const objetoImagen = JSON.parse(xhr.response)
+                this.imagenSubida = objetoImagen.nombre // {nombre: 'imagen.jpg'}
+            }
+        })
+
+        formData.append('foto', file);
+        xhr.send(formData)
+    }
+
+    /* -------------- drag and drop ----------------- */
 
 }
 
